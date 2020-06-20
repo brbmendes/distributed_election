7da84535804a1a3ff5d045f22ba9c0e6e50bb4a7
Bruno <bbmendes@gmail.com>
2020-06-20 19:38:54 -0300

    changed client and ip addr config

--- client.py
+++ client.py
@@ -1,23 +1,50 @@
 #!/usr/bin/env python
 
-import socket
+import socket,sys
 
+BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
-TCP_IP = "10.0.2.13"
-TCP_PORT = 25123
-BUFFER_SIZE = 1024
-MESSAGE = "Hello, World2!"
-print(MESSAGE)
-bytemessage = str.encode(MESSAGE)
-print("encoded")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("socket created")
-s.connect((TCP_IP, TCP_PORT))
-print("socket connected")
-s.send(bytemessage)
-print("sended")
-data = s.recv(BUFFER_SIZE)
-print("received")
-s.close()
+line = int(sys.argv[2])
 
-print ("received data:", data)
+f = open("arquivo_config.txt", "r")
+lines = f.read().splitlines()
+f.close()
+
+myInfo = lines[line-1]
+splittedInfo = myInfo.split(" ")
+myId = int(splittedInfo[0])
+myIp = splittedInfo[1]
+myPort = int(splittedInfo[2])
+print(myId)
+print(myIp)
+print(myPort)
+
+destInfo = lines[0]
+destSplittedInfo = myInfo.split(" ")
+destId = int(destSplittedInfo[0])
+destIp = destSplittedInfo[1]
+destPort = int(destSplittedInfo[2])
+
+while 1:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	print("socket created")
+	
+	s.connect((destIp, destPort))
+	print("socket connected")
+	
+	message = input('digite algo: ')
+	print(message)
+	bytemessage = str.encode(message)
+	print("encoded")
+	
+	s.send(bytemessage)
+	print("sended")
+
+	data = s.recv(BUFFER_SIZE)
+	print("received")
+	
+	s.close()
+	print("closed")
+
+	print ("received data:", data)
+
